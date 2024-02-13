61a1486b848a3b037eb677c7d8af0a31933bc3c8
Serhii Kaliuzhnyi <kayuzhni.sergei@gmail.com>
2024-02-13 18:44:21 +0200

    change cert manager server

--- kubernetes/lib/cert/index.ts
+++ kubernetes/lib/cert/index.ts
@@ -44,8 +44,8 @@ export class Cert extends Construct {
       spec: {
         acme: {
           email: 'kalyuzhni.sergei@gmail.com',
-          // server: 'https://acme-v02.api.letsencrypt.org/directory',
-          server: 'https://acme-staging-v02.api.letsencrypt.org/directory',
+          server: 'https://acme-v02.api.letsencrypt.org/directory',
+          // server: 'https://acme-staging-v02.api.letsencrypt.org/directory',
           privateKeySecretRef: {
             name: 'cert-manager-acme-private-key'
           },
