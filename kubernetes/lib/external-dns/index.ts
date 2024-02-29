fa2df91c403a18c3fc889ac14f16d5ba3b496fad
Serhii Kaliuzhnyi <kayuzhni.sergei@gmail.com>
2024-02-29 16:28:30 +0200

    comment external dns befor fixing irsa

--- kubernetes/lib/external-dns/index.ts
+++ kubernetes/lib/external-dns/index.ts
@@ -1,34 +1,34 @@
 import { Construct } from 'constructs';
-import { Helm } from 'cdk8s';
+// import { Helm } from 'cdk8s';
 
 export class ExternalDns extends Construct {
   constructor(scope: Construct, id: string) {
     super(scope, id);
-
-    new Helm(this, 'externaldns', {
-      chart: 'bitnami/external-dns',
-      repo: 'https://charts.bitnami.com/bitnami',
-      version: '6.34.2',
-      namespace: 'kube-system',
-      values: {
-        provider: 'aws',
-        aws: {
-          region: 'eu-central-1',
-          zoneType: 'public',
-          assumeRoleArn: 'arn:aws:iam::894208094359:role/ExternalDnsRole'
-        },
-        policy: 'upsert-only',
-        txtOwnerId: 'Z0807079F0FU5XCKLU6Z',
-        domainFilters: ['clickops.life'],
-        logLevel: 'info',
-        serviceAccount: {
-          name: 'external-dns',
-          annotations: {
-            'eks.amazonaws.com/role-arn': 'arn:aws:iam::894208094359:role/ExternalDnsRole',
-          }
-        }
-      },
-    });
+    // Fix IRSA
+    // new Helm(this, 'externaldns', {
+    //   chart: 'bitnami/external-dns',
+    //   repo: 'https://charts.bitnami.com/bitnami',
+    //   version: '6.34.2',
+    //   namespace: 'kube-system',
+    //   values: {
+    //     provider: 'aws',
+    //     aws: {
+    //       region: 'eu-central-1',
+    //       zoneType: 'public',
+    //       assumeRoleArn: 'arn:aws:iam::894208094359:role/ExternalDnsRole'
+    //     },
+    //     policy: 'upsert-only',
+    //     txtOwnerId: 'Z0807079F0FU5XCKLU6Z',
+    //     domainFilters: ['clickops.life'],
+    //     logLevel: 'info',
+    //     serviceAccount: {
+    //       name: 'external-dns',
+    //       annotations: {
+    //         'eks.amazonaws.com/role-arn': 'arn:aws:iam::894208094359:role/ExternalDnsRole',
+    //       }
+    //     }
+    //   },
+    // });
   }
 }
 
